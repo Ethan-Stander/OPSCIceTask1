d8eda89a310a385f32c770bbd5ed0a9d24733993
OofSpooder <ethanstander55@gmail.com>
2023-08-03 16:22:02 +0200

    Added joke class

---
+++ app/src/main/java/com/example/trumpdumpapi/JokeClass.kt
@@ -0,0 +1,8 @@
+package com.example.trumpdumpapi
+
+class JokeClass {
+    var jokeID: String? = null
+    var joke: String? = null
+    var author: String? = null
+    var picture: String? = null
+}
